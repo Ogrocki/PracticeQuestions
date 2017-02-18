17f24588386bd300677ebd4c95090415968c3709
Ogrocki <ogrocki@ualberta.ca>
2017-02-17 21:08:09 -0500

    added start of rect question

---
+++ rect.java
@@ -0,0 +1,15 @@
+//write a fuction that takes in two sets of x,y points which are
+// the bottom left and top right points of a rectangle.
+
+//Then find if those two rectangles are overlapping
+//return true on overlap
+//return false on not overlap
+
+public class rect{
+	public static void main(String[] args){
+
+	}
+public static boolean overlap(int bl1, int tr1, int bl2, int tr2){
+	
+}
+}
